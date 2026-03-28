293e6bef89e61274f7c30c92f7eaafaa7f03b0fd
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-28 11:23:58 +0530

    Add files via upload

---
+++ countwordslength.java
@@ -0,0 +1,8 @@
+public class countwordslength {
+    public static void main(String[]args){
+        String str="my name ram";
+        String wo[]=str.split(" ");
+        System.out.println(wo.length);
+    }
+    
+}
