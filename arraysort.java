ade423dc81296a4f6f1069739e456327280ccaff
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-28 11:21:27 +0530

    Add files via upload

---
+++ arraysort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arraysort {
+    public static void main(String[]args){
+        int arr[]={2,3,5,7,4,9};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            
+            System.out.print(arr[i]+" ");
+
+        }
+        //System.out.println(arr);
+
+    }
+    
+}
