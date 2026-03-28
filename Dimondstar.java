3bf0e71a9b8ccecb1b7ae81061f1aa666c10f43a
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-28 11:26:20 +0530

    Add files via upload

---
+++ Dimondstar.java
@@ -0,0 +1,25 @@
+public class Dimondstar {
+    public static void main(String[] args) {
+        int n=4;
+          for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }  
+   
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }     
+    }
+}
+   
