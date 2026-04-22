530df50d4afea8da1f41752e363ec1b2a6829ad5
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-04-22 11:34:25 +0530

    Add files via upload

---
+++ anagramusingfuncions.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class anagramusingfuncions {
+    public static boolean fun(HashMap<Character,Integer>hma,HashMap<Character,Integer>hmb){
+        if(hma.size()!=hmb.size()){
+            return false;
+        }
+        for(char key:hma.keySet()){
+            if(!hmb.containsKey(key)){
+                return false;
+            }
+            int a=hma.get(key);
+            int b=hmb.get(key);
+            if(a!=b){
+                return false;
+            }
+        }
+        return true;
+    }
+public static void main(String[]args){
+    String sa="abcde";
+    String sb="eabcd";
+    HashMap<Character,Integer>hma=new HashMap<>();
+    HashMap<Character,Integer>hmb=new HashMap<>();
+    for(int i=0;i<sa.length();i++){
+        char ch=sa.charAt(i);
+        hma.put(ch,hma.getOrDefault(ch,0)+1);
+
+    }
+    for(int i=0;i<sb.length();i++){
+        char ch=sb.charAt(i);
+        hmb.put(ch,hmb.getOrDefault(ch,0)+1);
+        
+    }
+    System.out.println(fun(hma,hmb));
+    
+   
+}
+}
